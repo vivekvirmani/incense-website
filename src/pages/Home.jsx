86c8765ec3893a66fc2757e9b848e95fade19bf7
vivek virmani <viveksinghvirmani@gmail.com>
2026-04-11 19:22:28 +0530

    testing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ import Footer from "../components/Footer"
 function Home() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       {/* <Hero /> */}
       {/* <About /> */}
       {/* <Products />
